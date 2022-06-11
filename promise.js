61bf35ae5c83585d6c558a2670664cab3134fdeb
Ifan Ramadhany <ifanpersie14@gmail.com>
2022-06-11 15:31:47 +0700

    challenge-07-done

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const getData = (cinameAttendanceData, emotion) => {
+  return new Promise((resolve, reject) => {
+    if (!emotion) {
+      reject("emotion is not found!")
+    } else {
+      let counter = 0;
+      for (let i = 0; i < cinameAttendanceData.length; i++) {
+        if (emotion == cinameAttendanceData[i].hasil) {
+          counter++;
+        }
+      }
+      resolve(counter);
+    }
+  })
+}
+
+const promiseOutput = async (emotion) => {
+  const theaterIXXCounter = await promiseTheaterIXX()
+    .then((cinameAttendanceData) => getData(cinameAttendanceData, emotion))
+  const theaterVGCCounter = await promiseTheaterVGC()
+    .then((cinameAttendanceData) => getData(cinameAttendanceData, emotion))
+  return theaterIXXCounter + theaterVGCCounter
+}
 
 module.exports = {
   promiseOutput,
